8f2d84e34b81e1280b3cd704041910a7c0ad0670
Benjamin Mahr <ben.mahr@gmail.com>
2019-02-12 07:23:38 +0100

    Build works now, but no tests executed

---
+++ src/solver/src/solver.cpp
@@ -0,0 +1,7 @@
+#include "solver.h"
+#include <iostream>
+
+void HelloWorld::helloWorld()
+{
+    std::cout << "Hello World!" << std::endl;
+};
---
+++ src/solver/test/solverTest.cpp
@@ -0,0 +1,6 @@
+#include <catch.hpp>
+#include "solver.h"
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( true );
+}
