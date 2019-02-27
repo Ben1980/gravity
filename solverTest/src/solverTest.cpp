d072c77c3fc1c455ae3a4be0d09eb71464e36d9a
Benjamin Mahr <ben.mahr@gmail.com>
2019-02-27 22:24:05 +0100

    small refactoring

--- solverTest/src/solverTest.cpp
+++ solverTest/src/solverTest.cpp
@@ -5,36 +5,60 @@
 #include "particleBuilder.h"
 #include <vector>
 
-TEST_CASE( "Explicit euler algorithm with two point mass", "[euler]" ) {
+double Inverse(double value) { return -value; }
 
-    Solver solver;
-
+Particle GenerateStandardParticle(double xPosition, double yPosition)
+{
     ParticleBuilder particleBuilder;
 
-    const Particle particleA = particleBuilder
-            .position({0.5, 0})
+    return particleBuilder
+            .position({xPosition, yPosition})
             .mass(1e10)
             .build();
+}
 
-    const Particle particleB = particleBuilder
-            .position({-0.5, 0})
-            .mass(1e10)
-            .build();
+TEST_CASE( "Explicit euler algorithm with two point mass", "[euler]" )
+{
+    Solver solver;
 
-    const std::vector<Particle> particles = { particleA, particleB };
+    const std::vector<Particle> particlesX = { GenerateStandardParticle(0.5, 0),
+                                               GenerateStandardParticle(-0.5, 0)};
+
+    const std::vector<Particle> particlesY = { GenerateStandardParticle(0, 0.5),
+                                               GenerateStandardParticle(0, -0.5)};
 
     const double EPSILON = 1e-3;
 
+    //Solution
+    const double acceleration = -0.6674079993; //m/s^2
+    const double velocity = -0.06674079993; //m/s
+    const double position = 0.48665184; //m
+
     SECTION( "Two still standing point mass are attracting each other in x-direction" ) {
-        const double acceleration = -0.6674079993; //m/s^2
-        const double velocity = -0.06674079993; //m/s
-        const double position = 0.48665184; //m
+        std::vector<Particle> result = solver.solve(particlesX, EPSILON);
 
-        std::vector<Particle> result = solver.solve(particles, EPSILON);
-
-        const Particle &particle = result[0];
+        Particle &particle = result.front();
         REQUIRE(particle.acceleration.x == Approx(acceleration));
         REQUIRE(particle.velocity.x == Approx(velocity));
         REQUIRE(particle.position.x == Approx(position));
+
+        particle = result.back();
+        REQUIRE(particle.acceleration.x == Approx(Inverse(acceleration)));
+        REQUIRE(particle.velocity.x == Approx(Inverse(velocity)));
+        REQUIRE(particle.position.x == Approx(Inverse(position)));
+    }
+
+    SECTION( "Two still standing point mass are attracting each other in y-direction" ) {
+        std::vector<Particle> result = solver.solve(particlesY, EPSILON);
+
+        Particle &particle = result.front();
+        REQUIRE(particle.acceleration.y == Approx(acceleration));
+        REQUIRE(particle.velocity.y == Approx(velocity));
+        REQUIRE(particle.position.y == Approx(position));
+
+        particle = result.back();
+        REQUIRE(particle.acceleration.y == Approx(Inverse(acceleration)));
+        REQUIRE(particle.velocity.y == Approx(Inverse(velocity)));
+        REQUIRE(particle.position.y == Approx(Inverse(position)));
     }
 }
